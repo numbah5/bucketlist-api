804596c1e478290e75dc5e6f3e72a77b1093bfbf
rishisurana13 <rishisurana@hotmail.com>
2019-03-29 11:14:03 -0400

    remove duplication

--- app/models/example.js
+++ app/models/example.js
@@ -1,64 +1,8 @@
-
-
-const mongoose = require('mongoose')
-
-const itemSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  text: {
-    type: String,
-    required: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  }
-}, {
-  timestamps: true
-}, {
-  completed: {
-    type: Boolean,
-    default: false
-  }
-})
-
-module.exports = mongoose.model('Item', itemSchema)
-const mongoose = require('mongoose')
-
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  hashedPassword: {
-    type: String,
-    required: true
-  },
-  token: String
-}, {
-  timestamps: true,
-  toObject: {
-    // remove `hashedPassword` field when we call `.toObject`
-    transform: (_doc, user) => {
-      delete user.hashedPassword
-      return user
-    }
-  }
-})
-
-module.exports = mongoose.model('User', userSchema)
-
+//
+//
 // const mongoose = require('mongoose')
 //
-// const exampleSchema = new mongoose.Schema({
+// const itemSchema = new mongoose.Schema({
 //   title: {
 //     type: String,
 //     required: true
@@ -67,6 +11,10 @@ module.exports = mongoose.model('User', userSchema)
 //     type: String,
 //     required: true
 //   },
+//   completed: {
+//     type: Boolean,
+//     default: false
+//   },
 //   owner: {
 //     type: mongoose.Schema.Types.ObjectId,
 //     ref: 'User',
@@ -74,6 +22,58 @@ module.exports = mongoose.model('User', userSchema)
 //   }
 // }, {
 //   timestamps: true
+// }, {
+//   completed: {
+//     type: Boolean,
+//     default: false
+//   }
 // })
 //
-// module.exports = mongoose.model('Example', exampleSchema)
+// module.exports = mongoose.model('Item', itemSchema)
+// const mongoose = require('mongoose')
+//
+// const userSchema = new mongoose.Schema({
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: true
+//   },
+//   hashedPassword: {
+//     type: String,
+//     required: true
+//   },
+//   token: String
+// }, {
+//   timestamps: true,
+//   toObject: {
+//     // remove `hashedPassword` field when we call `.toObject`
+//     transform: (_doc, user) => {
+//       delete user.hashedPassword
+//       return user
+//     }
+//   }
+// })
+//
+// module.exports = mongoose.model('User', userSchema)
+//
+// // const mongoose = require('mongoose')
+// //
+// // const exampleSchema = new mongoose.Schema({
+// //   title: {
+// //     type: String,
+// //     required: true
+// //   },
+// //   text: {
+// //     type: String,
+// //     required: true
+// //   },
+// //   owner: {
+// //     type: mongoose.Schema.Types.ObjectId,
+// //     ref: 'User',
+// //     required: true
+// //   }
+// // }, {
+// //   timestamps: true
+// // })
+// //
+// // module.exports = mongoose.model('Example', exampleSchema)
